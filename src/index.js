1234c810c56e01b57b984c93e7bff6ad8c352ace
Ilya Ivchenkov <gunti911@gmail.com>
2022-02-17 17:58:30 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let obj = {};
+
+    bracketsConfig.forEach(el => {
+        obj[el['0']] = el[1];
+    })
+
+    let bracketExample = Object.keys(obj);
+    let bracketValues = Object.values(obj);
+
+    for (let i = 0; i < str.length; i++) {
+        const bracket = str[i];
+        if (bracket === obj[stack[stack.length - 1]]) {
+            stack.pop()
+        } else if (bracketExample.indexOf(bracket) !== -1) {
+            stack.push(bracket);
+        } else if (bracketValues.indexOf(bracket) !== -1) {
+            return false;
+        }
+    }
+
+    if (!stack.length) {
+        return true;
+    } else {
+        return false;
+    }
 }
